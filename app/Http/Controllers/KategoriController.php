c9670bc88bd4d28bdc8e1a93a860501c09108655
ilhamGegana <geganailham@gmail.com>
2024-08-12 22:56:28 +0700

    Setelah Praktikum DB Facade dan Query Builder

---
+++ app/Http/Controllers/KategoriController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class KategoriController extends Controller
+{
+    public function index(){
+        //Menambah data
+        // $data = [
+        //     'kategori_kode'=>'SNK',
+        //     'kategori_nama'=>'Scank/Makanan ringan',
+        //     'created_at'=>now()
+        // ];
+        // DB::table('m_kategori')->insert($data);
+        // return 'Data berhasil ditambah';
+
+        //Update data
+        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama'=>'Camilan']);
+        // return 'Update data berhasil, jumlah data yang terupdate '.$row.' baris';
+
+        // Hapus data
+        // $data=DB::table('m_kategori')->where('kategori_kode','SNK')->delete();
+        // return 'Delete data berhasil, Jumlah data yang dihapus '.$data.' baris';
+
+        // Read data
+        $data=DB::table('m_kategori')->get();
+        return view('kategori',['data'=>$data]);
+    }
+}
